b8644784c7f6e4c45ace91d8a07f2d970561a178
Alexey Korepanov <grazder@yandex.ru>
2025-08-28 23:45:35 +0400

    fix

--- onnxruntime/core/providers/js/js_kernel.cc
+++ onnxruntime/core/providers/js/js_kernel.cc
@@ -3,6 +3,21 @@
 
 #include "js_kernel.h"
 
+EM_ASYNC_JS(
+    int,                                                    // возврат в C++
+    jsepKernelRun,                                          // имя функции
+    (intptr_t kernel_handle, intptr_t serialized_ctx_ptr),  // параметры
+    {
+      // Module.jsepRunKernelAsync должна вернуть Promise<int>
+      const status = await Module.jsepRunKernelAsync(
+          Number(kernel_handle),
+          Number(serialized_ctx_ptr),
+          Module.jsepSessionState.sessionHandle,
+          Module.jsepSessionState.errors);
+      // Приведение к C‑инту.
+      return Number(status);
+    });
+
 namespace onnxruntime {
 namespace js {
 }  // namespace js
--- onnxruntime/core/providers/js/js_kernel.h
+++ onnxruntime/core/providers/js/js_kernel.h
@@ -18,20 +18,8 @@ struct pthreadpool;
 namespace onnxruntime {
 namespace js {
 
-EM_ASYNC_JS(
-    int,                                                    // возврат в C++
-    jsepKernelRun,                                          // имя функции
-    (intptr_t kernel_handle, intptr_t serialized_ctx_ptr),  // параметры
-    {
-      // Module.jsepRunKernelAsync должна вернуть Promise<int>
-      const status = await Module.jsepRunKernelAsync(
-          Number(kernel_handle),
-          Number(serialized_ctx_ptr),
-          Module.jsepSessionState.sessionHandle,
-          Module.jsepSessionState.errors);
-      // Приведение к C‑инту.
-      return Number(status);
-    });
+extern "C" int jsepKernelRun(intptr_t kernel_handle,
+                             intptr_t serialized_ctx_ptr);
 
 // This macro is defined to bypass the code format from clang-format, which will overwrite "=>" into "= >"
 // We can use it to write JS inline code with arrow functions.
